6738c46af639e7bce9c539dc09b31f55cb3a4af6
Andrej-Orel <48686329+Andrej-Orel@users.noreply.github.com>
2019-07-22 12:54:24 +0300

    Literals

---
+++ FirstProg.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main(int argc, char *argv[])
+{
+  printf("This is integer literal  '10'\n");
+  printf("This is float literal '133,333'\n");;
+  printf("This is char literal char 'hello, a, b, c,\n");
+  printf("This is string literal 'hello world'\n");
+  std::cout << true << " " << false << std::endl;
+  return 0;
+}
