e8837bdcbab0eab038c0a9568782dc72a91289db
Andrej-Orel <48686329+Andrej-Orel@users.noreply.github.com>
2019-07-24 17:07:17 +0300

    Update Riv.cpp

--- Riv.cpp
+++ Riv.cpp
@@ -5,7 +5,7 @@
 int main() {
 	float a, b, c, D, x1, x2, x;
 	const int TWO=2,
-		      FOUR=4;
+		  FOUR=4;
 
 	std::cout << "Enter a="; std::cin >> a;
 	std::cout << "Enter b="; std::cin >> b;
@@ -14,16 +14,16 @@ int main() {
 	D = pow(b, TWO) - (FOUR * a*c);
 
 	if (D >= 0) {
-      if (a == 0) {
-        x = (-c) / b;
-		  std::cout << "x=" << x << std::endl;
+          if (a == 0) {
+            x = (-c) / b;
+              std::cout << "x=" << x << std::endl;
 	  }
-		else {
-		  x1 = (-b + sqrt(D)) / (TWO * a);
-		  x2 = (-b - sqrt(D)) / (TWO * a);
-		  std::cout << "x1=" << x1 << std::endl;
-		  std::cout << "x2=" << x2 << std::endl;
-		}
+	    else {
+              x1 = (-b + sqrt(D)) / (TWO * a);
+	      x2 = (-b - sqrt(D)) / (TWO * a);
+	      std::cout << "x1=" << x1 << std::endl;
+	      std::cout << "x2=" << x2 << std::endl;
+	    }
 	}
 	else {	printf("\n No root equation");	}
 
